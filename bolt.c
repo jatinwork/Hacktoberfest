4419486d579a23f4564242ba9376beb319597215
jatinwork <92011587+jatinwork@users.noreply.github.com>
2021-10-07 23:20:16 +0530

    variable change

--- bolt.c
+++ bolt.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 //void pattern(int);
 int main(){
-  int i=0;
-  scanf("%d",&i);
+  int k=0;
+  scanf("%d",&k);
   //input is taken from user
-  for(int j=0;j<i;j++){
+  for(int j=0;j<k;j++){
   {float k1,k2,k3,v;
   scanf("%f %f %f %f",&k1,&k2,&k3,&v);
   float n=k1*k2*k3*v;
